6fbd17a5a03116c9eaa8d64a0b81bbacfa4a39d9
Jon Steck <jonathan.steck@rakuten.com>
2018-10-02 11:37:33 -0700

    change jmeter location

--- pubmaticPOC/runLoadTest.sh
+++ pubmaticPOC/runLoadTest.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/data/apache-jmeter-5.0/bin/jmeter -Jhost=$SERVER -Jjmeter.save.saveservice.output_format=xml -n -t $WORKSPACE/$CONNECTOR/loadTest.jmx -l $WORKSPACE/$CONNECTOR/out.jtl
+jmeter -Jhost=$SERVER -Jjmeter.save.saveservice.output_format=xml -n -t $WORKSPACE/$CONNECTOR/loadTest.jmx -l $WORKSPACE/$CONNECTOR/out.jtl
