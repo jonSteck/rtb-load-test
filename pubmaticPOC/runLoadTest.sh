19a743908841eeb105525407e11b5c8de31e5fca
Jon Steck <jonathan.steck@rakuten.com>
2018-10-02 12:00:59 -0700

    jmeter location 2

--- pubmaticPOC/runLoadTest.sh
+++ pubmaticPOC/runLoadTest.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-jmeter -Jhost=$SERVER -Jjmeter.save.saveservice.output_format=xml -n -t $WORKSPACE/$CONNECTOR/loadTest.jmx -l $WORKSPACE/$CONNECTOR/out.jtl
+/data/apache-jmeter-5.0/bin/jmeter -Jhost=$SERVER -Jjmeter.save.saveservice.output_format=xml -n -t $WORKSPACE/$CONNECTOR/loadTest.jmx -l $WORKSPACE/$CONNECTOR/out.jtl
