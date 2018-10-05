a8ff71914f8587b4435d8dc86022ef0bf1bb20fa
Jon Steck <jonathan.steck@rakuten.com>
2018-10-05 11:46:11 -0700

    try shell expr

--- pubmaticPOC/runLoadTest.sh
+++ pubmaticPOC/runLoadTest.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-declare -i targetQPM
-targetQPM = $TARGET_QPS*60
+expr targetQPM = $TARGET_QPS \* 60
 echo $targetQPM
 
 /data/apache-jmeter-5.0/bin/jmeter -Jhost=$SERVER -JdurationSec=$TEST_DURATION_SEC -JtargetQPM=$targetQPM -Jjmeter.save.saveservice.output_format=xml -n -t $WORKSPACE/$CONNECTOR/loadTest.jmx -l $WORKSPACE/$CONNECTOR/out.jtl
