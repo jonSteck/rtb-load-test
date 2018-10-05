0f8090abf63a94336981e4ab6be6fe49f2ab16ff
Jon Steck <jonathan.steck@rakuten.com>
2018-10-05 11:34:39 -0700

    make qps configurable

--- pubmaticPOC/runLoadTest.sh
+++ pubmaticPOC/runLoadTest.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-/data/apache-jmeter-5.0/bin/jmeter -JdurationSec=$TEST_DURATION_SEC -Jhost=$SERVER -Jjmeter.save.saveservice.output_format=xml -n -t $WORKSPACE/$CONNECTOR/loadTest.jmx -l $WORKSPACE/$CONNECTOR/out.jtl
+declare -i targetQPM
+targetQPM = $TARGET_QPS/60
+echo $targetQPM
+
+/data/apache-jmeter-5.0/bin/jmeter -Jhost=$SERVER -JdurationSec=$TEST_DURATION_SEC -JtargetQPM=$targetQPM -Jjmeter.save.saveservice.output_format=xml -n -t $WORKSPACE/$CONNECTOR/loadTest.jmx -l $WORKSPACE/$CONNECTOR/out.jtl
